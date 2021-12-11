fa56a24703389fbc5b626d5985bc1fc15e81f958
vitaly97w <Vitaly97w@gmail.com>
2021-12-11 17:12:29 +1000

    jikhjkb

--- NomogramPrint/Servicies/ExcelParser.cs
+++ NomogramPrint/Servicies/ExcelParser.cs
@@ -14,10 +14,13 @@ namespace NomogramPrint.Servicies
     {
         public DbContext GetData()
         {
-            (int, int) km = getKm();
+            var km = getKm();
+            var iobject = getIObject();
             DbContext context = new DbContext();
             for (int i = km.Item2; i > km.Item1; i--)
-                context.Kilometers.Add(new Kilometer { Number = i, Digits = "00-00" });
+                context.Kilometers.Add(new Kilometer { Number = i});
+            for (int i = 0; i > iobject.Length; i++)
+                context.IObject.Add(new KTSM { Kilometers = iobject[i].Item1, Meters = iobject[i].Item2 });
             return context;
         }
         public Range readFile(string path)
@@ -40,6 +43,24 @@ namespace NomogramPrint.Servicies
             var endKm = (int)excelRange.Cells[1, 2].Value2;
             //after reading, relaase the excel project
             return (startKm, endKm);
+        }
+        public (int, int)[] getIObject()
+        {
+            Range excelRange = readFile(@"D:\hak\3_Komplexy_tekhnicheskikh_sredstv_monitoringa_KTSM.xlsx");
+            int rows = excelRange.Rows.Count;
+            int cols = excelRange.Columns.Count;
+            var km = new (int, int)[rows];
+            for (int i = 1; i <= rows; i++)
+            {
+                if (excelRange.Cells[i, 1] != null && excelRange.Cells[i, 1].Value2 != null)
+                    km[i].Item1 = (int)excelRange.Cells[i, 1].Value2;
+                if (excelRange.Cells[i, 2] != null && excelRange.Cells[i, 2].Value2 != null)
+                    km[i].Item2 = (int)excelRange.Cells[i, 2].Value2;
+
+            }
+            //after reading, relaase the excel project
+            return km;
+
         }
     }
 }
