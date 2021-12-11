14ad7b834b9eb76d438907221c27d3b3f829ad42
vitaly97w <Vitaly97w@gmail.com>
2021-12-11 13:40:26 +1000

    чтение файла км

--- NomogramPrint/Servicies/ExcelParser.cs
+++ NomogramPrint/Servicies/ExcelParser.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NomogramPrint.Models;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+
 
 namespace NomogramPrint.Servicies
 {
@@ -10,10 +14,32 @@ namespace NomogramPrint.Servicies
     {
         public DbContext GetData()
         {
+            (int, int) km = getKm();
             DbContext context = new DbContext();
-            for (int i = 380; i > 350; i--)
+            for (int i = km.Item2; i > km.Item1; i--)
                 context.Kilometers.Add(new Kilometer { Number = i, Digits = "00-00" });
             return context;
+        }
+        public Range readFile(string path)
+        {
+            //Create COM Objects.
+            Application excelApp = new Application();
+            Workbook excelBook = excelApp.Workbooks.Open(path);
+            _Worksheet excelSheet = excelBook.Sheets[1];
+            Range excelRange = excelSheet.UsedRange;
+            //excelApp.Quit();
+            //System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+            return excelRange;
+        }
+        public (int, int) getKm()
+        {
+            Range excelRange = readFile(@"D:\hak\Killometrazh.xlsx");
+            int rows = excelRange.Rows.Count;
+            int cols = excelRange.Columns.Count;
+            var startKm = (int)excelRange.Cells[1, 1].Value2;
+            var endKm = (int)excelRange.Cells[1, 2].Value2;
+            //after reading, relaase the excel project
+            return (startKm, endKm);
         }
     }
 }
